bf59e64a8bd21914334e4c145e3faa7249670079
Your Name <you@example.com>
2024-03-02 11:40:53 +0000

    modified accountSlice reducers and mechanism for setting local storage

--- src/components/account/SignupContainer.jsx
+++ src/components/account/SignupContainer.jsx
@@ -1,68 +1,48 @@
-import React, { useCallback } from "react";
+import React, { useState } from "react";
 import Label from "../genericComponents/Label";
 import Input from "../genericComponents/Input";
 import Button from "../genericComponents/Button";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  setSignupEmail,
-  setSignupUsername,
-  setSignupPassword,
-  selectSignupEmail,
-  selectSignupPassword,
-  selectSignupUsername,
-  setLocalStorage,
-} from "../../redux/accountSlice";
-import sha256 from "sha256";
+import { useDispatch } from "react-redux";
+import { setSignupDetails, setLocalStorage } from "../../redux/accountSlice";
 
 const SignupContainer = () => {
   const dispatch = useDispatch();
-  const email = useSelector(selectSignupEmail);
-  const username = useSelector(selectSignupUsername);
-  const password = useSelector(selectSignupPassword);
+  const [state, setState] = useState({});
+
+  const onInput = (e) => {
+    setState({ ...state, [e.target.id]: e.target.value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(setLocalStorage(email, username, password));
+    dispatch(setSignupDetails(state));
+    dispatch(setLocalStorage(state));
   };
 
   return (
-    <form className="signup-form">
+    <form className="signup-form" onInput={onInput}>
       <div className="signup-fields">
         <Label htmlFor="email" text="Email" />
-        <Input
-          type="text"
-          id="email"
-          name="email"
-          onInput={(e) => {
-            dispatch(setSignupEmail(e.target.value));
-          }}
-        />
+        <Input type="text" id="email" name="email" />
       </div>
+
       <div className="signup-fields">
-        <Label htmlFor="new-username" text="Enter chosen username" />
-        <Input
-          type="text"
-          id="new-username"
-          name="new-username"
-          onInput={(e) => {
-            dispatch(setSignupUsername(e.target.value));
-          }}
-        />
+        <Label htmlFor="signupUsername" text="Enter chosen username" />
+        <Input type="text" id="signupUsername" name="signupUsername" />
       </div>
+
       <div className="signup-fields">
-        <Label htmlFor="new-password" text="Password" />
+        <Label htmlFor="signupPassword" text="Password" />
+        <Input type="password" id="signupPassword" name="signupPassword" />
+      </div>
+
+      <div className="signup-fields">
+        <Label htmlFor="passwordConfirmation" text="Confirm Password" />
         <Input
           type="password"
-          id="new-password"
-          name="new-password"
-          onInput={(e) => {
-            dispatch(setSignupPassword(sha256(e.target.value)));
-          }}
+          id="passwordConfirmation"
+          name="cpasswordConfirmation"
         />
-      </div>
-      <div className="signup-fields">
-        <Label htmlFor="confirm-password" text="Confirm Password" />
-        <Input type="password" id="confirm-password" name="confirm-password" />
       </div>
       <Button
         className="signup-button button"
