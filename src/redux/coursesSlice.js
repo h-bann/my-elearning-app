0afc787336f667dcbc1adc73aa358f2bd859de49
Your Name <you@example.com>
2024-04-01 18:45:31 +0100

    added functionality to allow user to leave a course and linked to back end

--- src/redux/coursesSlice.js
+++ src/redux/coursesSlice.js
@@ -1,9 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { storeMultipleInLocal } from "../storage";
 
 const initialState = {
   coursesScreen: 0,
-  myLearning: [],
+  courses: [],
 };
 
 export const coursesSlice = createSlice({
@@ -22,14 +21,6 @@ export const coursesSlice = createSlice({
     setMoreInfoContent: (state, { payload }) => {
       state.moreInfo = payload;
     },
-    setMyLearning: (state, { payload }) => {
-      const isDuplicate = state.myLearning.some(
-        (item) => item.id === payload.id
-      );
-      if (!isDuplicate) {
-        return { ...state, myLearning: [...state.myLearning, payload] };
-      }
-    },
   },
 });
 
@@ -38,7 +29,6 @@ export const {
   setModuleContent,
   setCourseContent,
   setMoreInfoContent,
-  setMyLearning,
 } = coursesSlice.actions;
 
 // * this is how you retrieve from store
@@ -47,6 +37,5 @@ export const selectCourses = (state) => state.courses.courses;
 export const selectModuleContent = (state) => state.courses.moduleContent;
 export const selectCourseContent = (state) => state.courses.courseContent;
 export const selectMoreInfoContent = (state) => state.courses.moreInfo;
-export const seclectMyLearning = (state) => state.courses.myLearning;
 
 export default coursesSlice.reducer;
