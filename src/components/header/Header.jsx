e6cd7fa87471c5aef549a73d60794010d1f701bd
Your Name <you@example.com>
2024-05-26 12:29:05 +0100

    style changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -19,10 +19,39 @@ const Header = () => {
           </Navbar.Brand>
         </div>
 
-        <dic className="navbar">
-          <Navigation />
-          <HeaderButtons />
-        </dic>
+        <div className="navbar">
+          <svg
+            class="burger-menu"
+            width="30px"
+            height="3 0px"
+            viewBox="0 0 24 24"
+            fill="#000000"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 18L20 18"
+              stroke="#000000"
+              stroke-width="2"
+              stroke-linecap="round"
+            />
+            <path
+              d="M4 12L20 12"
+              stroke="#000000"
+              stroke-width="2"
+              stroke-linecap="round"
+            />
+            <path
+              d="M4 6L20 6"
+              stroke="#000000"
+              stroke-width="2"
+              stroke-linecap="round"
+            />
+          </svg>
+          <div className="dropdown">
+            <Navigation />
+            <HeaderButtons />
+          </div>
+        </div>
       </Nav>
     </header>
   );
