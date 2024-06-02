789f43d8690401761f63541813f990dec011530d
Harry Bannister <harry_bannister@outlook.com>
2024-06-02 19:23:16 +0100

    style changes

--- src/components/main/ModuleContent.jsx
+++ src/components/main/ModuleContent.jsx
@@ -72,8 +72,6 @@ const ModuleContent = ({ moduleId }) => {
   if (!moduleContent || state === null) {
     <p>Loading</p>;
   }
-  // console.log(moduleContent);
-  // console.log(state);
 
   if (innerWidth < 365) {
     return (
@@ -86,7 +84,28 @@ const ModuleContent = ({ moduleId }) => {
                 key={item.id}
                 onClick={() => onModuleClick(item)}
               >
-                <h1>{item.module_title}</h1>
+                <div className="module-tabs">
+                  <h1>{item.module_title}</h1>
+                  <div
+                    className={`svg-container ${
+                      state && state !== item.id && "rotated"
+                    }`}
+                  >
+                    <svg
+                      width="25px"
+                      height="25px"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="down-arrow"
+                    >
+                      <path
+                        d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z"
+                        fill="#0F0F0F"
+                      />
+                    </svg>
+                  </div>
+                </div>
                 <div
                   className={`content ${
                     state && state !== item.id && "hidden"
@@ -106,10 +125,7 @@ const ModuleContent = ({ moduleId }) => {
                       case "list":
                         return (
                           <>
-                            {/* {content.map((item) => {
-                  return <li>{item}</li>;
-                })} */}
-                            <li key={id} className="ms-3">
+                            <li key={id} className="main-list-item">
                               {content}
                             </li>
                           </>
@@ -118,10 +134,7 @@ const ModuleContent = ({ moduleId }) => {
                       case "subList":
                         return (
                           <>
-                            {/* {content.map((item) => {
-                  return <li>{item}</li>;
-                })} */}
-                            <li key={id} className="ms-5">
+                            <li key={id} className="sub-list-item">
                               {content}
                             </li>
                           </>
