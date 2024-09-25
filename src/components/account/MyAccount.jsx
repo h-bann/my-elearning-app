7197c92da84e9df23418145be6a44fbba55ae442
Harry <harry_bannister@outlook.com>
2024-09-25 18:05:43 +0100

    updated user details form functionality

--- src/components/account/MyAccount.jsx
+++ src/components/account/MyAccount.jsx
@@ -4,11 +4,7 @@ import { useDispatch } from "react-redux";
 import Button from "../genericComponents/Button";
 import Input from "../genericComponents/Input";
 import Label from "../genericComponents/Label";
-import {
-  contactFormSchema,
-  formValidation,
-  userDetailsResetSchema,
-} from "../../utils/Joi";
+import { formValidation, userDetailsResetSchema } from "../../utils/Joi";
 import axios from "axios";
 import { getFromLocal, clearLocal } from "../../storage";
 import { url } from "../../config";
@@ -21,7 +17,6 @@ const MyAccount = () => {
   const navigate = useNavigate();
   const [display, setDisplay] = useState("");
   const [errors, setErrors] = useState("");
-  const [userInput, setUserInput] = useState();
   const [userDetails, setUserDetails] = useState();
   const [userEmail, setUserEmail] = useState();
   const [userUsername, setUserUsername] = useState();
@@ -46,7 +41,6 @@ const MyAccount = () => {
     formValidation(userInput, userDetailsResetSchema, setErrors);
     switch (e.target.name) {
       case "email":
-        // formValidation(userInput, userDetailsResetSchema, setErrors);
         setUserEmail(e.target.value);
         break;
       case "username":
@@ -65,26 +59,10 @@ const MyAccount = () => {
         null;
         break;
     }
-
-    // const updatedState = { ...userInput, [e.target.name]: e.target.value };
-    // formValidation(updatedState, userDetailsResetSchema, setErrors);
-    // setUserInput(updatedState);
   };
-  // console.log(userEmail);
-  // console.log(userUsername);
-  // console.log(userCurrentPassword);
-  // console.log(userPassword);
-  console.log(errors);
-
-  // console.log(userPasswordConfirmation);
 
   const updateUserDetails = async (e) => {
     e.preventDefault();
-    // spread userInput so as only to send newState to database
-    // const { passwordConfirmation, currentPassword, ...newState } = userInput;
-    // const { data } = await axios.patch(`${url}/users/update`, newState, {
-    //   headers: { token: getFromLocal("token") },
-    // });
 
     const { data } = await axios.patch(
       `${url}/users/update`,
@@ -103,9 +81,8 @@ const MyAccount = () => {
     if (data.code === 1) {
       setDisplay(null);
     }
-    // if action fails, display error message and set user input to null so that correct message is displayed
+    // if action fails, send error message to state
     if (data.code === 0) {
-      setUserInput(null);
       setErrors(data.message);
     }
     if (data.code === 2) {
@@ -176,11 +153,6 @@ const MyAccount = () => {
               {email && display === "email" && errors && (
                 <p className="form-text">{errors}</p>
               )}
-              {/* {email && display === "email" && errors.email ? (
-                <p className="form-text">{errors.email}</p>
-              ) : (
-                <p className="form-text">{errors}</p>
-              )} */}
               <Button
                 className={["btn-primary"]}
                 type="submit"
@@ -315,172 +287,8 @@ const MyAccount = () => {
         </div>
       </form>
 
-      {/* <form
-        className="form-signin"
-        onInput={onInput}
-        onSubmit={updateUserDetails}
-      >
-        <div className="details-container">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <div className="flex">
-            <DownArrow className="arrow rotated-right" />
-            <p>{email}</p>
-          </div>
-          {display != "email" && (
-            <Button
-              className={["btn-primary", "account"]}
-              onClick={() => {
-                setDisplay("email");
-                setErrors("");
-              }}
-              text="Change Email"
-            />
-          )}
-          {display === "email" && (
-            <div className="hidden-input">
-              <Input
-                type="email"
-                name="email"
-                placeholder="New email address"
-                className="form-control"
-              />
-              {email && display === "email" && errors.email ? (
-                <p className="form-text">{errors.email}</p>
-              ) : (
-                <p className="form-text">{errors}</p>
-              )}
-              <Button
-                className={["btn-primary"]}
-                type="submit"
-                text="Save"
-                disabled={!userInput || errors ? true : false}
-              />
-            </div>
-          )}
-        </div>
-
-        <div className="details-container">
-          <label htmlFor="username" className="form-label">
-            Username
-          </label>
-          <div className="flex">
-            <DownArrow className="arrow rotated-right" />
-            <p>{username}</p>
-          </div>
-          {display != "username" && (
-            <Button
-              className={["btn-primary", "account"]}
-              onClick={() => {
-                setDisplay("username");
-                setErrors("");
-              }}
-              text="Change username"
-            />
-          )}
-          {display === "username" && (
-            <div className="hidden-input">
-              <Input
-                type="text"
-                name="username"
-                placeholder="New username"
-                className="form-control"
-              />
-              {username && display === "username" && errors.username ? (
-                <p className="form-text">{errors.username}</p>
-              ) : (
-                <p className="form-text">
-                  {typeof errors === "object" ? JSON.stringify(errors) : errors}
-                </p>
-              )}
-              <Button
-                className={["btn-primary"]}
-                type="submit"
-                text="Save"
-                disabled={!userInput || errors ? true : false}
-              />
-            </div>
-          )}
-        </div>
-
-        <div className="details-container password">
-          <label htmlFor="password" className="form-label">
-            Password Reset
-          </label>
-          {display != "password" && (
-            <Button
-              className={["btn-primary", "account"]}
-              onClick={() => {
-                setDisplay("password");
-                setErrors("");
-              }}
-              text="Reset password"
-            />
-          )}
-          {display === "password" && (
-            <div className="hidden-input">
-              <Label
-                htmlFor="password"
-                className="sub-form-label"
-                text="Current password:"
-              />
-              <Input
-                type="password"
-                name="currentPassword"
-                placeholder="Current password"
-                className="form-control mb-3"
-              />
-
-              <Label
-                htmlFor="newPassword"
-                className="sub-form-label"
-                text="New password:"
-              />
-              <Input
-                type="password"
-                name="password"
-                placeholder="New password"
-                className="form-control mb-3"
-              />
-              {password && display === "password" && errors.password ? (
-                <p className="form-text">{errors.password}</p>
-              ) : errors ? (
-                <p className="form-text">{errors}</p>
-              ) : undefined}
-
-              <Label
-                htmlFor="passwordConfirmation"
-                text="Confirm new password:"
-                className="sub-form-label"
-              />
-              <Input
-                type="password"
-                name="passwordConfirmation"
-                placeholder="Confirm new password"
-                className="form-control mb-3"
-              />
-              {userInput &&
-                userInput.passwordConfirmation != userInput.password && (
-                  <p className="form-text">Passwords do not match</p>
-                )}
-              <Button
-                className={["btn-primary"]}
-                type="submit"
-                text="Save"
-                disabled={
-                  !userInput ||
-                  errors ||
-                  userInput.passwordConfirmation != userInput.password
-                    ? true
-                    : false
-                }
-              />
-            </div>
-          )}
-        </div>
-      </form> */}
       <div className="p-3">
+        <p className="form-label">Account Deletion</p>
         <Button
           className={["btn-danger"]}
           type="button"
