a4c807485bc25958434c866a4472ba9659005669
Harry <harry_bannister@outlook.com>
2024-09-25 17:51:44 +0100

    updated user details form functionality

--- src/components/account/MyAccount.jsx
+++ src/components/account/MyAccount.jsx
@@ -4,7 +4,11 @@ import { useDispatch } from "react-redux";
 import Button from "../genericComponents/Button";
 import Input from "../genericComponents/Input";
 import Label from "../genericComponents/Label";
-import { formValidation, userDetailsResetSchema } from "../../utils/Joi";
+import {
+  contactFormSchema,
+  formValidation,
+  userDetailsResetSchema,
+} from "../../utils/Joi";
 import axios from "axios";
 import { getFromLocal, clearLocal } from "../../storage";
 import { url } from "../../config";
@@ -22,6 +26,7 @@ const MyAccount = () => {
   const [userEmail, setUserEmail] = useState();
   const [userUsername, setUserUsername] = useState();
   const [userPassword, setUserPassword] = useState();
+  const [userCurrentPassword, setUserCurrentPassword] = useState();
   const [userPasswordConfirmation, setUserPasswordConfirmation] = useState();
 
   useEffect(() => {
@@ -39,7 +44,6 @@ const MyAccount = () => {
   const onInput = (e) => {
     const userInput = { [e.target.name]: e.target.value };
     formValidation(userInput, userDetailsResetSchema, setErrors);
-    console.log(e.target);
     switch (e.target.name) {
       case "email":
         // formValidation(userInput, userDetailsResetSchema, setErrors);
@@ -47,6 +51,9 @@ const MyAccount = () => {
         break;
       case "username":
         setUserUsername(e.target.value);
+        break;
+      case "currentPassword":
+        setUserCurrentPassword(e.target.value);
         break;
       case "password":
         setUserPassword(e.target.value);
@@ -65,9 +72,11 @@ const MyAccount = () => {
   };
   // console.log(userEmail);
   // console.log(userUsername);
+  // console.log(userCurrentPassword);
   // console.log(userPassword);
   console.log(errors);
-  console.log(userPasswordConfirmation);
+
+  // console.log(userPasswordConfirmation);
 
   const updateUserDetails = async (e) => {
     e.preventDefault();
@@ -79,21 +88,28 @@ const MyAccount = () => {
 
     const { data } = await axios.patch(
       `${url}/users/update`,
-      { email: userEmail, username: userUsername, password: userPassword },
+      {
+        email: userEmail,
+        username: userUsername,
+        currentPassword: userCurrentPassword,
+        password: userPassword,
+      },
       {
         headers: { token: getFromLocal("token") },
       }
     );
 
     // if action is successful, reset display to start
-    if (data.code) {
+    if (data.code === 1) {
       setDisplay(null);
     }
     // if action fails, display error message and set user input to null so that correct message is displayed
-    if (!data.code) {
+    if (data.code === 0) {
       setUserInput(null);
       setErrors(data.message);
-      console.log(data.message);
+    }
+    if (data.code === 2) {
+      setErrors(data);
     }
   };
 
@@ -142,6 +158,7 @@ const MyAccount = () => {
               onClick={() => {
                 setUserUsername("");
                 setUserPassword("");
+                setUserCurrentPassword("");
                 setDisplay("email");
                 setErrors("");
               }}
@@ -156,13 +173,14 @@ const MyAccount = () => {
                 className="form-control"
                 placeholder="New email address"
               />
-              {userEmail && display === "email" && errors.email ? (
+              {email && display === "email" && errors && (
+                <p className="form-text">{errors}</p>
+              )}
+              {/* {email && display === "email" && errors.email ? (
                 <p className="form-text">{errors.email}</p>
               ) : (
-                <p className="form-text">
-                  {typeof errors === "object" ? JSON.stringify(errors) : errors}
-                </p>
-              )}
+                <p className="form-text">{errors}</p>
+              )} */}
               <Button
                 className={["btn-primary"]}
                 type="submit"
@@ -187,6 +205,7 @@ const MyAccount = () => {
               onClick={() => {
                 setUserEmail("");
                 setUserPassword("");
+                setUserCurrentPassword("");
                 setDisplay("username");
                 setErrors("");
               }}
@@ -201,12 +220,8 @@ const MyAccount = () => {
                 placeholder="New username"
                 className="form-control"
               />
-              {username && display === "username" && errors.username ? (
-                <p className="form-text">{errors.username}</p>
-              ) : (
-                <p className="form-text">
-                  {typeof errors === "object" ? JSON.stringify(errors) : errors}
-                </p>
+              {username && display === "username" && errors && (
+                <p className="form-text">{errors}</p>
               )}
               <Button
                 className={["btn-primary"]}
@@ -237,7 +252,7 @@ const MyAccount = () => {
           {display === "password" && (
             <div className="hidden-input">
               <Label
-                htmlFor="password"
+                htmlFor="currentPassword"
                 className="sub-form-label"
                 text="Current password:"
               />
@@ -247,9 +262,12 @@ const MyAccount = () => {
                 placeholder="Current password"
                 className="form-control mb-3"
               />
+              {password && display === "password" && errors.code === 2 && (
+                <p className="form-text">{errors.message}</p>
+              )}
 
               <Label
-                htmlFor="newPassword"
+                htmlFor="password"
                 className="sub-form-label"
                 text="New password:"
               />
@@ -259,9 +277,11 @@ const MyAccount = () => {
                 placeholder="New password"
                 className="form-control mb-3"
               />
-              {password && display === "password" && errors.password ? (
-                <p className="form-text">{errors.password}</p>
-              ) : undefined}
+              {password &&
+                display === "password" &&
+                typeof errors !== "object" && (
+                  <p className="form-text">{errors}</p>
+                )}
 
               <Label
                 htmlFor="passwordConfirmation"
@@ -283,6 +303,7 @@ const MyAccount = () => {
                 text="Save"
                 disabled={
                   !userPassword ||
+                  !userCurrentPassword ||
                   errors ||
                   userPasswordConfirmation != userPassword
                     ? true
